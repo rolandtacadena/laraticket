17bb01dc7bd27587111f47e462dd17b1acb13f3a
Roland Tacadena <tacadena.roland@gmail.com>
2016-08-18 20:18:43 +0800

    Updating db seeds

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -15,6 +15,7 @@ class CommentsTableSeeder extends Seeder
         //factory(Comment::class, 10)->create();
         for($i = 1; $i <= 10; $i++) {
             DB::table('comments')->insert([
+                'id' => $i,
                 'comment' => str_random(30),
                 'user_id' => 1,
                 'ticket_id' => 1,
