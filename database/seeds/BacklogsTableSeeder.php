a73827436ced25d4570bd156d179e2458cb32507
Roland Tacadena <tacadena.roland@gmail.com>
2016-08-18 20:17:35 +0800

    Updating db seeds

--- database/seeds/BacklogsTableSeeder.php
+++ database/seeds/BacklogsTableSeeder.php
@@ -16,6 +16,7 @@ class BacklogsTableSeeder extends Seeder
         //factory(Backlog::class, 10)->create();
         for($i = 1; $i <= 10; $i++) {
             DB::table('backlogs')->insert([
+                'id' => $i,
                 'name' => str_random(15),
                 'description' => str_random(40),
             ]);
